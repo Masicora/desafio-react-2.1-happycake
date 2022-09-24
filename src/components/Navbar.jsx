56a8d57558453dc85f6f87758bc888ae7b848ed8
Jonathan Bidwell <jbidwellb@gmail.com>
2022-09-24 15:41:36 -0300

    commit inicial

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Navbar, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default function Navigation() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container className="justify-content-start">
+          <Navbar.Brand>Dogstel</Navbar.Brand>
+          <Link to="/" className="text-white ms-3 text-decoration-none">Home</Link>
+          <Link to="/planes" className="text-white ms-3 text-decoration-none">Planes</Link>
+          <Link to="/login" className="text-white ms-3 text-decoration-none">Login</Link>
+          <Link to="/registro" className="text-white ms-3 text-decoration-none">Registro</Link>
+
+        </Container>
+      </Navbar>
+    </>
+  );
+}
